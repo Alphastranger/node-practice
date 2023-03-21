22cd1ea1be1b2924a587fab6a55e020bcc9cee3d
Alpha <121796109+Alphastranger@users.noreply.github.com>
2023-03-21 19:08:04 -0400

    start project

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require('inquirer')
+
+inquirer.prompt ([
+    {
+        type: 'list',
+        name: 'reptile',
+        message: 'Which is better?',
+        choices: ['alligator', 'crocodile'],
+    }
+])
